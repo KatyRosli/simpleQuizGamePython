7acabed0cc4c716b658fc4407c3d4c73ec4f586f
Katy Rosli <katyrosli@gmail.com>
2023-06-26 14:33:09 +0200

    game works

--- quiz_game.py
+++ quiz_game.py
@@ -3,34 +3,34 @@ print("Welcome to my computer qiz!")
 # note: there's a space so user can type after the question mark
 playing = input("Do you want to play? ")
 
-# if statement
-if playing != "yes":
+# if statement & string covert to lowercase this way we accepting many version of yes
+if playing.lower() != "yes":
     quit()
 
 # if true then print statement
 print("Okey! Let's play :)")
 
 answer = input("What does CPU stand for? ")
-if answer == "central procession unit":
+if answer.lower() == "central procession unit":
     # note: accept single qoute as well
     print('Correct!')
 else:
     print('Incorrect')
 
 answer = input("What does GPU stand for? ")
-if answer == "graphics procession unit":
+if answer.lower() == "graphics procession unit":
     print('Correct!')
 else:
     print('Incorrect')
 
 answer = input("What does RAM stand for? ")
-if answer == "random access memory":
+if answer.lower() == "random access memory":
     print('Correct!')
 else:
     print('Incorrect')
 
 answer = input("What does PSU stand for? ")
-if answer == "power supply":
+if answer.lower() == "power supply":
     print('Correct!')
 else:
     print('Incorrect')
