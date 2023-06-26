17cdcd03f1e7a715818f5951667002209b2bfdac
Katy Rosli <katyrosli@gmail.com>
2023-06-26 14:41:24 +0200

    added scores

--- quiz_game.py
+++ quiz_game.py
@@ -10,27 +10,36 @@ if playing.lower() != "yes":
 # if true then print statement
 print("Okey! Let's play :)")
 
+score = 0
+
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central procession unit":
     # note: accept single qoute as well
     print('Correct!')
+    score += 1
 else:
     print('Incorrect')
 
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics procession unit":
     print('Correct!')
+    score += 1
 else:
     print('Incorrect')
 
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
     print('Correct!')
+    score += 1
 else:
     print('Incorrect')
 
 answer = input("What does PSU stand for? ")
 if answer.lower() == "power supply":
     print('Correct!')
+    score += 1
 else:
     print('Incorrect')
+
+# convert number to string
+print("You got " + str(score) + " questions correct!")
