5b8f9e696466583b1bd711e5f1cfdfe1b3008bf4
Katy Rosli <katyrosli@gmail.com>
2023-06-29 20:04:54 +0200

    started a new game password manager

--- choose_adventure_game.py
+++ choose_adventure_game.py
@@ -5,7 +5,7 @@ answer = input(
     "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
 
 if answer == "left":
-    answer = input("You come to a river, you can walk around it or swim across? Type walk to walk around and swim to swim accross.")
+    answer = input("You come to a river, you can walk around it or swim across? Type walk to walk around and swim to swim accross. ")
 
     # checks users answers
     if answer == "swim":
@@ -16,13 +16,13 @@ if answer == "left":
         print("Not a valid option. You lose.")
 
 elif answer == "right":
-    answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)?")
+    answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ")
 
     # checks users answers
     if answer == "back":
         print("You go back and lose.")
     elif answer == "cross":
-        answer = input("You cross the bridge and meet a stranger. Do you talk to them (yes/no)?")
+        answer = input("You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ")
 
         if answer == "yes":
             print("You talk to the stranger and they give you gold. YOU WIN!")
