93af412c2fe63f6f66a5c79d554970c00e7c74dd
Katy Rosli <katyrosli@gmail.com>
2023-07-03 11:16:10 +0200

    encoded and decoded pwd

--- password_manager.py
+++ password_manager.py
@@ -1,7 +1,5 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("what is the master password? ")
-
 '''
 # this is a function that creates a key file
 def write_key():
@@ -11,6 +9,17 @@ def write_key():
     with open("key.key", "wb") as key_file:
         key_file.write(key) '''
 
+# this is a function that loads the key file
+def load_key():
+    # "rb" option opens the file in binary format for reading
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+# encode() method encodes the string, using the specified encoding
+key = load_key()
+fer = Fernet(key)
 
 # this is a function
 def view():
@@ -24,9 +33,12 @@ def view():
             # rstrip means removes any trailing characters (characters at the end a string), 
             # space is the default trailing character to remove
             data = line.rstrip()
+            if "|" not in data:
+                continue
             # this means it will remove "|" 
             user, passw = data.split("|")
-            print("User:", user, "| Password:", passw)
+            print("User:", user, "| Password:", 
+                  fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input('Account Name: ')
@@ -39,7 +51,7 @@ def add():
     # The data being written will be inserted at the end, after the existing data.
     with open('passswords.txt', 'a') as f:
         # file name is f
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
